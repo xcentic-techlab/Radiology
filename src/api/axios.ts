9f26004b423ec44a190ae922f7a2d37a82bbdd76
RaghavRR <raghavrastogi2021@gmail.com>
2025-12-07 22:17:15 +0530

    fix api proxy

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,19 +2,19 @@ import axios from "axios";
 import { useAuthStore } from "@/store/authStore";
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000",
-  
+  // baseURL: import.meta.env.VITE_API_URL || "http://localhost:4000",
+  baseURL: "/api",
 });
+
 
 
 
 
 instance.interceptors.request.use((config) => {
   const token = useAuthStore.getState().token;
-  console.log("REQUEST URL:", config.baseURL + config.url);
-  console.log("ENV API URL:", import.meta.env.VITE_API_URL);
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
 
 export default instance;
+
